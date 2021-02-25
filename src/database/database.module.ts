e6e77cf031b8ab0548ec2d83ea204df6ee27714c
Daniel Magee <Dmagee2184@conestogac.on.ca>
2021-02-24 22:00:16 -0500

    added user class + boilerplate

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { rootCertificates } from 'tls';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'flashPack',
+      synchronize: true,
+    }),
+  ],
+})
 export class DatabaseModule {}
